8756c90ddb0e3b1128ffe78ce71400077976bdbc
Steven Lundy <steven.lundy@gmail.com>
2015-07-06 13:26:27 -0400

    Wrote uniq function

--- src/underbar.js
+++ src/underbar.js
@@ -105,6 +105,15 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var unique = [];
+
+    _.each(array, function(item){
+      if(_.indexOf(unique, item) === -1){
+        unique.push(item);
+      }
+    });
+
+    return unique;
   };
 
 
