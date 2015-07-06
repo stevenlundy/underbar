49ff29816c01f74e868e1da9c6d65b045725b918
Steven Lundy <steven.lundy@gmail.com>
2015-07-06 13:15:39 -0400

    Wrote filter function

--- src/underbar.js
+++ src/underbar.js
@@ -83,6 +83,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var matches = [];
+
+    _.each(collection, function(item){
+      if(test(item)){
+        matches.push(item);
+      }
+    });
+
+    return matches;
   };
 
   // Return all elements of an array that don't pass a truth test.
