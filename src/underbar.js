81cb26d7310c02d8bc009ca7ef35994776510af3
Steven Lundy <steven.lundy@gmail.com>
2015-07-09 19:00:06 -0400

    Wrote sortBy function for when iterator is a function

--- src/underbar.js
+++ src/underbar.js
@@ -387,7 +387,13 @@
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
     if(typeof(iterator) === 'function'){
-
+      for(var i = 1; i < collection.length; i++){
+        for(var j = i; j > 0 && iterator(collection[j]) < iterator(collection[j-1]); j--){
+          var temp = collection[j];
+          collection[j] = collection[j-1];
+          collection[j-1] = temp;
+        }
+      }
     } else {
       for(var i = 1; i < collection.length; i++){
         for(var j = i; collection[j][iterator] < collection[j-1][iterator] && j > 0; j--){
