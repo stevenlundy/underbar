38992adf9b6596967f6a921a908680e27efff559
Steven Lundy <steven.lundy@gmail.com>
2015-07-09 19:08:53 -0400

    Refactored sortBy function to include a swap function.

--- src/underbar.js
+++ src/underbar.js
@@ -386,20 +386,21 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    var swap = function(a,b){
+      var temp = collection[a];
+      collection[a] = collection[b];
+      collection[b] = temp;
+    }
     if(typeof(iterator) === 'function'){
       for(var i = 1; i < collection.length; i++){
         for(var j = i; j > 0 && (collection[j-1] === undefined || iterator(collection[j]) < iterator(collection[j-1])); j--){
-          var temp = collection[j];
-          collection[j] = collection[j-1];
-          collection[j-1] = temp;
+          swap(j, j-1);
         }
       }
     } else {
       for(var i = 1; i < collection.length; i++){
         for(var j = i; j > 0 && (collection[j-1] === undefined || collection[j][iterator] < collection[j-1][iterator]); j--){
-          var temp = collection[j];
-          collection[j] = collection[j-1];
-          collection[j-1] = temp;
+          swap(j, j-1);
         }
       }
     }
