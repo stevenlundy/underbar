590d3342079b367d2ed8f170e8122ad793b3dcb0
Steven Lundy <steven.lundy@gmail.com>
2015-07-09 18:54:25 -0400

    Wrote sortBy function that works when iterator is a string

--- src/underbar.js
+++ src/underbar.js
@@ -386,6 +386,18 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    if(typeof(iterator) === 'function'){
+
+    } else {
+      for(var i = 1; i < collection.length; i++){
+        for(var j = i; collection[j][iterator] < collection[j-1][iterator] && j > 0; j--){
+          var temp = collection[j];
+          collection[j] = collection[j-1];
+          collection[j-1] = temp;
+        }
+      }
+    }
+    return collection;
   };
 
   // Zip together two or more arrays with elements of the same index
